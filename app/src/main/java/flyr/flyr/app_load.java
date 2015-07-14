d5c64bde242f9496e109a1a3ace23dd1c2cdc721
funkyrishi <funkyrishi@gmail.com>
2015-07-14 18:51:42 +0530

    Initial commit

---
+++ app/src/main/java/flyr/flyr/app_load.java
@@ -0,0 +1,15 @@
+package flyr.flyr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class app_load extends Activity {
+    private static final String TAG = app_load.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_load);
+    }
+}
